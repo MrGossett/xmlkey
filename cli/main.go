e212e742a26efc3d6f812b12af6b95b8dd0f1a1a
Tim Gossett <tim@cloud.com>
2015-09-22 21:49:28 -0400

    add flags for cli

--- cli/main.go
+++ cli/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"crypto/x509"
 	"encoding/pem"
+	"flag"
 	"io/ioutil"
 	"log"
 	"os"
@@ -11,7 +12,12 @@ import (
 )
 
 func main() {
-	bs, err := ioutil.ReadAll(os.Stdin)
+	var in, out string
+	flag.StringVar(&in, "in", "STDIN", "The file containing the XML key to parse")
+	flag.StringVar(&out, "out", "STDOUT", "The file into which the PEM-encoded key should be written")
+	flag.Parse()
+
+	bs, err := ioutil.ReadAll(infile(in))
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -21,11 +27,37 @@ func main() {
 		log.Fatal(err)
 	}
 
-	err = pem.Encode(os.Stdout, &pem.Block{
+	err = pem.Encode(outfile(out), &pem.Block{
 		Bytes: x509.MarshalPKCS1PrivateKey(key),
 		Type:  "RSA PRIVATE KEY",
 	})
 	if err != nil {
 		log.Fatal(err)
 	}
+}
+
+func outfile(path string) *os.File {
+	if path == "STDOUT" {
+		return os.Stdout
+	}
+
+	outFile, err := os.Open(path)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return outFile
+}
+
+func infile(path string) *os.File {
+	if path == "STDIN" {
+		return os.Stdin
+	}
+
+	inFile, err := os.Open(path)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return inFile
 }
